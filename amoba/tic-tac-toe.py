ff9d420d06ccbb9592b1da60666f88cfa0c321c9
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2024-12-19 18:03:15 +0100

    Update tic-tac-toe.py

--- amoba/tic-tac-toe.py
+++ amoba/tic-tac-toe.py
@@ -1,75 +1,92 @@
-lista = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+# megvizsgálja hogy az adott területre lehet-e alakzatot tenni
+def felulir(tabla: list[list[str]], oszlop: int, sor: int) -> bool:
+    return tabla[sor][oszlop] == "-"
 
 
-def felulir(lista, oszlop, sor):
-    if lista[sor][oszlop] == "-":
-        return True
-    else:
-        return False
+# kiírja a tábla aktuális állását
+def kiirando(tabla: list[list[str]]) -> None:
+    for sor in tabla:
+        print(*sor)
 
 
-def kiirando(lista):
-    for i in lista:
-        print(*i)
-
-
-def van_e_gyoztes(lista):
-    for i in lista:
-        if i.count("X") == 3 or i.count("O") == 3:
+def van_e_gyoztes(tabla: list[list[str]]) -> bool:
+    # Ellenőrizzük a sorokat és oszlopokat
+    for i in range(3):
+        if tabla[i] == ['X', 'X', 'X'] or tabla[i] == ['O', 'O', 'O']:  # Sorok
             return True
-    for i in range(0, 3):
-        segito = []
-        for y in lista:
-            segito.append(y[i])
-        if segito.count("X") == 3 or segito.count("O") == 3:
+        if [tabla[j][i] for j in range(3)] in [['X', 'X', 'X'], ['O', 'O', 'O']]:  # Oszlopok
             return True
-    db = 0
-    segito = []
-    while db != 3:
-        for i in range(db, db + 1):
-            for y in range(db, db + 1):
-                segito.append(lista[i][y])
 
-        db += 1
-    if segito.count("X") == 3 or segito.count("O") == 3:
+    # Ellenőrizzük az átlókat
+    if [tabla[i][i] for i in range(3)] in [['X', 'X', 'X'], ['O', 'O', 'O']]:  # Főátló
+        return True
+    if [tabla[i][2 - i] for i in range(3)] in [['X', 'X', 'X'], ['O', 'O', 'O']]:  # Mellékátló
         return True
 
-    segito = []
-    segito.append(lista[0][2])
-    segito.append(lista[1][1])
-    segito.append(lista[2][0])
-    if segito.count("X") == 3 or segito.count("O") == 3:
-        return True
     return False
 
 
-kezdes = 1
-db = 0
-while True:
-    try:
-        bekert_adat = list(map(int, input().split()))
-        if felulir(lista, bekert_adat[1] - 1, bekert_adat[0] - 1):
-            db += 1
-            if kezdes == 1:
-                lista[bekert_adat[0] - 1][bekert_adat[1] - 1] = "X"
-                kezdes = 0
-                kiirando(lista)
+def jatek() -> None:
+    # tábla létrehozása
+    tabla: list[list[str]] = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
+
+    # kezdeti változók beállítása
+
+    kezdes: int = 1
+    db: int = 0
+    fut: bool = True
+
+    # játékciklus
+    while fut:
+        try:
+
+            # bekérjük a játékos pozícióját
+            bekert_adat = list(map(int, input().split('Add meg a pozíciót, ahova pakolni szeretnél')))
+
+            # megvizsgáljuk lehet-e oda tenni alakzatot
+            if felulir(tabla, bekert_adat[1] - 1, bekert_adat[0] - 1):
+                db += 1
+
+                # X kezd majd letesszük az alakzatot, kiírjuk a táblát és váltunk
+                if kezdes % 2 == 1:
+                    tabla[bekert_adat[0] - 1][bekert_adat[1] - 1] = "X"
+                    kezdes += 1
+                    kiirando(tabla)
+
+                # O tesz majd letesszük az alakzatot, kiírjuk a táblát és váltunk
+                else:
+                    tabla[bekert_adat[0] - 1][bekert_adat[1] - 1] = "O"
+                    kezdes += 1
+                    kiirando(tabla)
+
+                # megvizsgáljuk van-e győztes
+                if van_e_gyoztes(tabla):
+                    if kezdes % 2 == 1:
+                        print("O nyert")
+                    else:
+                        print("X nyert")
+
+                    fut = False
+
+                if db == 9:
+                    print("Döntetlen")
+                    fut = False
+
+                print()
 
             else:
-                lista[bekert_adat[0] - 1][bekert_adat[1] - 1] = "O"
-                kezdes = 1
-                kiirando(lista)
-            if van_e_gyoztes(lista):
-                if kezdes == 1:
-                    print("O nyert")
-                else:
-                    print("X nyert")
-                break
-            if db == 9:
-                print("Döntetlen")
-                break
-            print()
-        else:
-            print("Az adott hely már foglalt, próbáld meg újra!")
-    except IndexError:
-        print("Túlmentél kolléga/Hibás adatot adtál meg")
+                print("Az adott hely már foglalt, próbáld meg újra!")
+
+        except IndexError:
+            print("Túlmentél kolléga/Hibás adatot adtál meg")
+
+        except ValueError:
+            print("Csak számokat adj meg! Próbáld újra!")
+
+
+def main() -> None:
+    jatek()
+
+
+if __name__ == '__main__':
+    main()
