4bd0142e55344ba2d93e38c1606638e8f9b7bc11
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2024-12-20 22:55:51 +0100

    Update lvl1.py

--- platformer_game/lvl1.py
+++ platformer_game/lvl1.py
@@ -10,48 +10,49 @@ def lvl1() -> None:
     pygame.mixer.init()
 
     # hangok importálása
-    explosions = pygame.mixer.Sound('hangok/bomba_robbanas.wav')
-    punch = pygame.mixer.Sound('hangok/punch.mp3')
-    teleporting = pygame.mixer.Sound('hangok/teleport.mp3')
+    explosions: pygame.mixer.Sound = pygame.mixer.Sound('hangok/bomba_robbanas.wav')
+    punch: pygame.mixer.Sound = pygame.mixer.Sound('hangok/punch.mp3')
+    teleporting: pygame.mixer.Sound = pygame.mixer.Sound('hangok/teleport.mp3')
 
     # Képernyő beállítása
-    screen_width, screen_height = 800, 600
-    screen = pygame.display.set_mode((screen_width, screen_height))
+    screen_width: int = 800
+    screen_height: int = 600
+    screen: pygame.Surface = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Platformer Game Demo")
 
     # Színek
-    blue = (0, 0, 255)
-    brown = (150, 75, 0)
-    green = (34, 139, 34)
+    blue: tuple[int, int, int] = (0, 0, 255)
+    brown: tuple[int, int, int] = (150, 75, 0)
+    green: tuple[int, int, int] = (34, 139, 34)
 
     # Zászló betöltése és átméretezése
-    original_flag_image = pygame.image.load('kepek/flag.png').convert_alpha()
-    new_flag_image = pygame.transform.scale(original_flag_image, (80, 160)).convert_alpha()
-    flag_rect = new_flag_image.get_rect()
+    original_flag_image: pygame.Surface = pygame.image.load('kepek/flag.png').convert_alpha()
+    new_flag_image: pygame.Surface = pygame.transform.scale(original_flag_image, (80, 160)).convert_alpha()
+    flag_rect: pygame.Rect = new_flag_image.get_rect()
     flag_rect.topleft = (70, 5)
 
     # Teleportok betöltése, átméretezése
-    original_tp_image = pygame.image.load('kepek/teleport.png').convert_alpha()
-    new_tp_image = pygame.transform.scale(original_tp_image, (40, 120)).convert_alpha()
-    tp_rect1 = new_tp_image.get_rect()
+    original_tp_image: pygame.Surface = pygame.image.load('kepek/teleport.png').convert_alpha()
+    new_tp_image: pygame.Surface = pygame.transform.scale(original_tp_image, (40, 120)).convert_alpha()
+    tp_rect1: pygame.Rect = new_tp_image.get_rect()
     tp_rect1.topleft = (750, 433)
-    tp_rect2 = new_tp_image.get_rect()
+    tp_rect2: pygame.Rect = new_tp_image.get_rect()
     tp_rect2.topleft = (0, 225)
 
     # szív kirajzolása
-    original_hp_image = pygame.image.load('kepek/heart.png').convert_alpha()
-    new_hp_image = pygame.transform.scale(original_hp_image, (75, 75)).convert_alpha()
-    hp_rect = new_hp_image.get_rect()
+    original_hp_image: pygame.Surface = pygame.image.load('kepek/heart.png').convert_alpha()
+    new_hp_image: pygame.Surface = pygame.transform.scale(original_hp_image, (75, 75)).convert_alpha()
+    hp_rect: pygame.Rect = new_hp_image.get_rect()
     hp_rect.topleft = (15, 30)
 
     # Játékos példányosítása
-    player = Player(0, 500, 50, 50)
+    player: Player = Player(0, 500, 50, 50)
 
     # Élet
-    lives = 20
+    lives: int = 20
 
     # Platformok
-    platforms = [
+    platforms: list[pygame.Rect] = [
         pygame.Rect(0 + 0 * 150, 550, 50, 50), pygame.Rect(0 + 1 * 150, 550, 50, 50),
         pygame.Rect(0 + 2 * 150, 550, 50, 50), pygame.Rect(0 + 3 * 150, 550, 50, 50),
         pygame.Rect(0 + 4 * 150, 550, 50, 50), pygame.Rect(0 + 5 * 150, 550, 50, 50),
@@ -61,36 +62,39 @@ def lvl1() -> None:
     ]
 
     # fű kirajzolása
-    grasses = [pygame.Rect(0 + 0 * 150, 550, 50, 25), pygame.Rect(0 + 1 * 150, 550, 50, 25),
-               pygame.Rect(0 + 2 * 150, 550, 50, 25), pygame.Rect(0 + 3 * 150, 550, 50, 25),
-               pygame.Rect(0 + 4 * 150, 550, 50, 25), pygame.Rect(0 + 5 * 150, 550, 50, 25),
-               pygame.Rect(0, 340, 200, 25), pygame.Rect(330, 340, 200, 25), pygame.Rect(650, 340, 200, 25),
-               pygame.Rect(700, 290, 50, 25), pygame.Rect(750, 240, 50, 25),
-               pygame.Rect(0, 160, 700, 12.5)
-               ]
+    grasses: list[pygame.Rect] = [
+        pygame.Rect(0 + 0 * 150, 550, 50, 25), pygame.Rect(0 + 1 * 150, 550, 50, 25),
+        pygame.Rect(0 + 2 * 150, 550, 50, 25), pygame.Rect(0 + 3 * 150, 550, 50, 25),
+        pygame.Rect(0 + 4 * 150, 550, 50, 25), pygame.Rect(0 + 5 * 150, 550, 50, 25),
+        pygame.Rect(0, 340, 200, 25), pygame.Rect(330, 340, 200, 25), pygame.Rect(650, 340, 200, 25),
+        pygame.Rect(700, 290, 50, 25), pygame.Rect(750, 240, 50, 25),
+        pygame.Rect(0, 160, 700, 12.5)
+    ]
 
     # ellenfelek példányosítása és tárolása
-    enemies = [JumpingEnemy(100, 500, 30, 30), JumpingEnemy(250, 455, 30, 30), JumpingEnemy(400, 485, 30, 30),
-               JumpingEnemy(550, 385, 30, 30), JumpingEnemy(700, 375, 30, 30),
-               JumpingEnemy(570, 250, 30, 30), JumpingEnemy(250, 300, 30, 30)
-               ]
+    enemies: list[JumpingEnemy] = [
+        JumpingEnemy(100, 500, 30, 30), JumpingEnemy(250, 455, 30, 30), JumpingEnemy(400, 485, 30, 30),
+        JumpingEnemy(550, 385, 30, 30), JumpingEnemy(700, 375, 30, 30),
+        JumpingEnemy(570, 250, 30, 30), JumpingEnemy(250, 300, 30, 30)
+    ]
 
     # bombák példányosítása és tárolása
-    bombs = [Bombs(600, 130, 30, 30), Bombs(400, 130, 30, 30), Bombs(200, 130, 30, 30),
-             Bombs(500, 130, 30, 30), Bombs(300, 130, 30, 30),
-             ]
+    bombs: list[Bombs] = [
+        Bombs(600, 130, 30, 30), Bombs(400, 130, 30, 30), Bombs(200, 130, 30, 30),
+        Bombs(500, 130, 30, 30), Bombs(300, 130, 30, 30),
+    ]
 
     # Játék ciklus
-    clock = pygame.time.Clock()
+    clock: pygame.time.Clock = pygame.time.Clock()
 
     # sikeresen befejeződött_e a játék?
-    successful = False
+    successful: bool = False
 
     # Alapértelmezett betűtípus beállítása
-    font = pygame.font.Font(None, 36)
+    font: pygame.font.Font = pygame.font.Font(None, 36)
 
     # A játék futása
-    run = True
+    run: bool = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -100,7 +104,7 @@ def lvl1() -> None:
                     run = False
 
         # játékosmozgás
-        keys = pygame.key.get_pressed()
+        keys: pygame.key.ScancodeWrapper = pygame.key.get_pressed()
         player.move(keys, screen_width)
         player.apply_gravity()
         player.check_collision(platforms)
@@ -121,6 +125,8 @@ def lvl1() -> None:
 
         for grass in grasses:
             pygame.draw.rect(screen, green, grass)
+
+        # ellenfelek kirajzolása és mozgatása és
 
         # ellenfelek kirajzolása és mozgatása és ütközések kezelése
         for index, enemy in enumerate(enemies, 1):
