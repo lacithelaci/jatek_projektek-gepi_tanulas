9399ef7aaeb99b2ca40718cbe7e747084336b1a5
lacithelaci <68651344+lacithelaci@users.noreply.github.com>
2024-12-20 22:56:21 +0100

    Update lvl2.py

--- platformer_game/lvl2.py
+++ platformer_game/lvl2.py
@@ -11,81 +11,92 @@ def lvl2() -> None:
     pygame.mixer.init()
 
     # hangok importálása
-    explosions = pygame.mixer.Sound('hangok/bomba_robbanas.wav')
-    punch = pygame.mixer.Sound('hangok/punch.mp3')
-    teleporting = pygame.mixer.Sound('hangok/teleport.mp3')
-    burning = pygame.mixer.Sound('hangok/burn.wav')
+    explosions: pygame.mixer.Sound = pygame.mixer.Sound('hangok/bomba_robbanas.wav')
+    punch: pygame.mixer.Sound = pygame.mixer.Sound('hangok/punch.mp3')
+    teleporting: pygame.mixer.Sound = pygame.mixer.Sound('hangok/teleport.mp3')
+    burning: pygame.mixer.Sound = pygame.mixer.Sound('hangok/burn.wav')
 
     # Képernyő beállítása
-    screen_width, screen_height = 800, 600
-    screen = pygame.display.set_mode((screen_width, screen_height))
+    screen_width: int = 800
+    screen_height: int = 600
+    screen: pygame.Surface = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Platformer Game Demo")
 
     # Színek
-    blue = (0, 0, 255)
-    brown = (150, 75, 0)
+    blue: tuple[int, int, int] = (0, 0, 255)
+    brown: tuple[int, int, int] = (150, 75, 0)
 
     # Zászló betöltése és átméretezése
-    original_flag_image = pygame.image.load('kepek/flag.png').convert_alpha()
-    new_flag_image = pygame.transform.scale(original_flag_image, (80, 160)).convert_alpha()
+    original_flag_image: pygame.Surface = pygame.image.load('kepek/flag.png').convert_alpha()
+    new_flag_image: pygame.Surface = pygame.transform.scale(original_flag_image, (80, 160)).convert_alpha()
     new_flag_image = pygame.transform.flip(new_flag_image, True, False)
-    flag_rect = new_flag_image.get_rect()
+    flag_rect: pygame.Rect = new_flag_image.get_rect()
     flag_rect.topleft = (80, 160)
 
     # Teleportok betöltése, átméretezése
-    original_tp_image = pygame.image.load('kepek/teleport.png').convert_alpha()
-    new_tp_image = pygame.transform.scale(original_tp_image, (40, 120)).convert_alpha()
-    tp_rect1 = new_tp_image.get_rect()
+    original_tp_image: pygame.Surface = pygame.image.load('kepek/teleport.png').convert_alpha()
+    new_tp_image: pygame.Surface = pygame.transform.scale(original_tp_image, (40, 120)).convert_alpha()
+    tp_rect1: pygame.Rect = new_tp_image.get_rect()
     tp_rect1.topleft = (750, 433)
-    tp_rect2 = new_tp_image.get_rect()
+    tp_rect2: pygame.Rect = new_tp_image.get_rect()
     tp_rect2.topleft = (0, 22)
 
     # szív kirajzolása
-    original_hp_image = pygame.image.load('kepek/heart.png').convert_alpha()
-    new_hp_image = pygame.transform.scale(original_hp_image, (75, 75)).convert_alpha()
-    hp_rect = new_hp_image.get_rect()
+    original_hp_image: pygame.Surface = pygame.image.load('kepek/heart.png').convert_alpha()
+    new_hp_image: pygame.Surface = pygame.transform.scale(original_hp_image, (75, 75)).convert_alpha()
+    hp_rect: pygame.Rect = new_hp_image.get_rect()
     hp_rect.topleft = (15, 200)
 
-    # platformok kirajzolásaq
-    platforms = [pygame.Rect(0, 550, 150, 50), pygame.Rect(300, 550, 150, 50), pygame.Rect(600, 550, 200, 50),
-                 pygame.Rect(0, 320, 200, 25), pygame.Rect(330, 320, 200, 25), pygame.Rect(650, 320, 200, 25),
-                 pygame.Rect(0, 140, 700, 25)]
+    # platformok kirajzolása
+    platforms: list[pygame.Rect] = [
+        pygame.Rect(0, 550, 150, 50), pygame.Rect(300, 550, 150, 50), pygame.Rect(600, 550, 200, 50),
+        pygame.Rect(0, 320, 200, 25), pygame.Rect(330, 320, 200, 25), pygame.Rect(650, 320, 200, 25),
+        pygame.Rect(0, 140, 700, 25)
+    ]
 
     # tűzlabdák kirajzolása
-    fireballs = [FireBall(25, 25, (225, 450), 80, 0.075, math.radians(0)),
-                 FireBall(25, 25, (225, 450), 80, 0.075, math.radians(180)),
-                 FireBall(25, 25, (525, 450), 80, 0.075, math.radians(0)),
-                 FireBall(25, 25, (525, 450), 80, 0.075, math.radians(180))]
+    fireballs: list[FireBall] = [
+        FireBall(25, 25, (225, 450), 80, 0.075, math.radians(0)),
+        FireBall(25, 25, (225, 450), 80, 0.075, math.radians(180)),
+        FireBall(25, 25, (525, 450), 80, 0.075, math.radians(0)),
+        FireBall(25, 25, (525, 450), 80, 0.075, math.radians(180))
+    ]
 
     # ellenfelek példányosítása és tárolása
-    enemies = [JumpingEnemy(570, 250, 30, 30), JumpingEnemy(250, 300, 30, 30)]
+    enemies: list[JumpingEnemy] = [
+        JumpingEnemy(570, 250, 30, 30), JumpingEnemy(250, 300, 30, 30)
+    ]
 
     # tüskés ellenfél példányosítása
-    spikes = [Spike(300, 295, 25, 25, 320, 530, 6),
-              Spike(150, 115, 25, 25, 100, 700, 5),
-              Spike(350, 115, 25, 25, 100, 700, 5),
-              Spike(600, 115, 25, 25, 100, 700, 5), ]
+    spikes: list[Spike] = [
+        Spike(300, 295, 25, 25, 320, 530, 6),
+        Spike(150, 115, 25, 25, 100, 700, 5),
+        Spike(350, 115, 25, 25, 100, 700, 5),
+        Spike(600, 115, 25, 25, 100, 700, 5)
+    ]
 
     # bombák példányosítása és tárolása
-    bombs = [Bombs(250, 0, 30, 30), Bombs(500, 0, 30, 30)]
+    bombs: list[Bombs] = [
+        Bombs(250, 0, 30, 30), Bombs(500, 0, 30, 30)
+    ]
 
     # Játékos példányosítása
-    player = Player(0, 500, 50, 50)
+    player: Player = Player(0, 500, 50, 50)
 
     # élet
-    lives = 50
+    lives: int = 50
 
     # Alapértelmezett betűtípus beállítása
-    font = pygame.font.Font(None, 36)
+    font: pygame.font.Font = pygame.font.Font(None, 36)
 
     # sikeresen befejeződött-e a játék?
-    succesfull = False
+    succesfull: bool = False
 
     # Játék ciklus
-    clock = pygame.time.Clock()
+    clock: pygame.time.Clock = pygame.time.Clock()
 
     # A játék futása
-    run = True
+    run: bool = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
